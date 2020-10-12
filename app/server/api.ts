261dfdc76276acfb2cdf8ac8ce4c32814f091e1d
Martin Szarski <mszarski@gmail.com>
2020-10-12 20:58:04 +1100

    Fixed CORS in api

--- app/server/api.ts
+++ app/server/api.ts
@@ -11,6 +11,8 @@ import {
   deleteRecord,
 } from './db';
 
+var cors = require('cors')
+
 const app = express();
 
 function asyncWrapper(fn) {
@@ -21,9 +23,11 @@ function asyncWrapper(fn) {
   };
 }
 
+app.options('*', cors())
+
 app.use(bodyParser.json({ limit: '200mb' }));
 
-app.post('/documents', (req: any, res: any) => {
+app.post('/documents', cors(), (req: any, res: any) => {
   if (Object.keys(req.body).length !== 0 && req.body.constructor === Object) {
     for (const key in req.body) {
       if (req.body[key].title && req.body[key].excerpt) {
@@ -35,7 +39,7 @@ app.post('/documents', (req: any, res: any) => {
   res.sendStatus(200);
 });
 
-app.put('/documents', (req: any, res: any) => {
+app.put('/documents', cors(), (req: any, res: any) => {
   console.log(req.body);
   if (req.body.length > 0) {
     req.body.forEach((doc) => {
@@ -46,7 +50,7 @@ app.put('/documents', (req: any, res: any) => {
   res.sendStatus(200);
 });
 
-app.get('/documents/:documentId', async (req: any, res: any) => {
+app.get('/documents/:documentId', cors(), async (req: any, res: any) => {
   if (req.params && req.params.documentId) {
     const { documentId } = req.params;
     console.log('getting key', documentId, req.body);
@@ -58,7 +62,7 @@ app.get('/documents/:documentId', async (req: any, res: any) => {
   res.send(500);
 });
 
-app.post('/documents/:documentId', (req: any, res: any) => {
+app.post('/documents/:documentId', cors(), (req: any, res: any) => {
   if (req.params && req.params.documentId) {
     const { documentId } = req.params;
     console.log('updating key', documentId, req.body);
@@ -67,7 +71,7 @@ app.post('/documents/:documentId', (req: any, res: any) => {
   res.sendStatus(200);
 });
 
-app.post('/tags', (req: any, res: any) => {
+app.post('/tags', cors(), (req: any, res: any) => {
   if (req.body && req.body.color && req.body.label) {
     console.log(req.body);
     addRecord(`tag_${req.body.color}_${req.body.label}`, req.body);
@@ -75,7 +79,7 @@ app.post('/tags', (req: any, res: any) => {
   res.sendStatus(200);
 });
 
-app.post('/notes', (req: any, res: any) => {
+app.post('/notes', cors(), (req: any, res: any) => {
   if (req.body) {
     console.log(req.body);
     addRecord(req.body.id, req.body);
@@ -83,7 +87,7 @@ app.post('/notes', (req: any, res: any) => {
   res.sendStatus(200);
 });
 
-app.delete('/notes/:notesId', (req: any, res: any) => {
+app.delete('/notes/:notesId', cors(), (req: any, res: any) => {
   if (req.params && req.params.notesId) {
     const { notesId } = req.params;
     console.log('deleting key', notesId, req.body);
@@ -92,7 +96,7 @@ app.delete('/notes/:notesId', (req: any, res: any) => {
   res.sendStatus(200);
 });
 
-app.put('/notes/:notesId', (req: any, res: any) => {
+app.put('/notes/:notesId', cors(), (req: any, res: any) => {
   try {
     if (req.params && req.params.notesId) {
       const { notesId } = req.params;
@@ -105,10 +109,10 @@ app.put('/notes/:notesId', (req: any, res: any) => {
   }
 });
 
-app.get('/documents', asyncWrapper(fetchRecords));
+app.get('/documents', cors(), asyncWrapper(fetchRecords));
 
-app.get('/tags', asyncWrapper(fetchTags));
+app.get('/tags', cors(), asyncWrapper(fetchTags));
 
-app.get('/notes', asyncWrapper(fetchNotes));
+app.get('/notes', cors(), asyncWrapper(fetchNotes));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
